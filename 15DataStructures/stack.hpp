74c295710a5d691d877ee4ff8385d74abcb7f02a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-07-12 17:35:09 -0400

    added 'List<STACKTYPE>::' to Stack class def function bodies to resolve namespace

--- 15DataStructures/stack.hpp
+++ 15DataStructures/stack.hpp
@@ -8,10 +8,10 @@
 template<class STACKTYPE>
 class Stack : private List<STACKTYPE> {
 public:
-  void push(const STACKTYPE& d) { insertAtFront(d); }
-  bool pop(STACKTYPE& d) { return removeFromFront(d); }
-  bool isStackEmpty() const { return isEmpty(); }
-  void printStack() const { print(); }
+  void push(const STACKTYPE& d) { List<STACKTYPE>::insertAtFront(d); }
+  bool pop(STACKTYPE& d) { return List<STACKTYPE>::removeFromFront(d); }
+  bool isStackEmpty() const { return List<STACKTYPE>::isEmpty(); }
+  void printStack() const { List<STACKTYPE>::print(); }
 };
 
 #endif
