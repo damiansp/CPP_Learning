892ba7412e23fd829825a9b31c5451ec3a1cff4e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-21 00:45:54 -0500

    add sum

---
+++ deitel/howto/02intro/sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+  int n1;
+  int n2;
+  int sum;
+  std::cout << "Enter first int: ";
+  std::cin >> n1;
+  std::cout << "Enter second int: ";
+  std::cin >> n2;
+  sum = n1 + n2;
+  std::cout << "Sum is: " << sum << std::endl;
+  return 0;
+}
