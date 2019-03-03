e92fa6ccc919c9edae9e6a6771d09f6c983fe7c4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-02 21:49:32 -0500

    GradeBOok takes course name param

--- deitel/howto/03classesObjects/GradeBook.cpp
+++ deitel/howto/03classesObjects/GradeBook.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
+
+#include <string>
+using std::string;
+using std::getline;
 
 
 class GradeBook {
 public:
-  void displayMessage() {
-    cout << "Welcome to the Grade Book!" << endl;
+  void displayMessage(string courseName) {
+    cout << "Welcome to the Grade Book for\n" << courseName << "!" << endl;
   }
 };
 
 
 int main() {
+  string courseName;
   GradeBook myGradeBook;
-  myGradeBook.displayMessage();
+
+  cout << "Name of course: " << endl;
+  getline(cin, courseName);
+  myGradeBook.displayMessage(courseName);
   return 0;
 }
