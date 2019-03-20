8dfadf4d63e0974c89aa67fa96c9a7eb52b5ce78
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-20 08:46:33 -0700

    rm makefiles dir

--- makefiles/separate_compilation.sh
+++
@@ -1,9 +0,0 @@
-# Compile
-g++ -c main.cpp       # makes main.o
-g++ -c Point.cpp      # makes Point.o
-g++ -c Rectangle.cpp  # makes Rectangle.o
-
-# Link
-g++ -o main.exe main.o Point.o Rectangle.o # makes executable main.exe
-
-
