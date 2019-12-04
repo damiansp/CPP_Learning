82414d58c44f17b29847f18c2499e69403c608b4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-12-04 07:38:40 -0800

    added solve() and dependencies

--- coursera/OODataStructures/04_templateTypes/Game.cpp
+++ coursera/OODataStructures/04_templateTypes/Game.cpp
@@ -18,3 +18,32 @@ Game::Game() {
   stacks_[0].pushBack(purple);
   stacks_[0].pushBack(yellow);
 }
+
+
+void Game::_move(unsigned p1, unsigned p2) {
+  Cube cube = stacks_[p1].removeTop();
+  stacks_[p2].pushBack(cube);
+}
+
+
+void Game::_legalMove(unsigned p1, unsigned p2) {
+  if (stacks_[p1].size() == 0 && stacks_[p2].size() > 0 ) {
+    _move(p2, p1);
+  } else if (stacks_[p1].size() > 0 && stacks_[p2].size() == 0) {
+    _move(p1, p2);
+  } else if (stacks_[p1].size() > 0 && stacks_[p2].size() > 0) {
+    if (stacks_[p1].peekTop().getLength() < stacks_[p2].peekTop().getLength()) {
+      _move(p1, p2);
+    } else { _move(p2, p1); }
+  }
+  cout << *this << endl;
+}
+
+
+void Game::solve() {
+  while (stacks_[2].size != 4) {
+    _legalMove(0, 1);
+    _legalMove(0, 2);
+    _legalMove(1, 2);
+  }
+}
