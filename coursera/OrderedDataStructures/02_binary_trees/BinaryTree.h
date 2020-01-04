748a0f273810319429962d5a6e1ff32698371aee
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-01-04 12:27:12 -0800

    add binary tree snippet

---
+++ coursera/OrderedDataStructures/02_binary_trees/BinaryTree.h
@@ -0,0 +1,16 @@
+template <typename T>
+class BinaryTree {
+ public:
+  // ...
+
+ private:
+  class TreeNode {
+  public:
+    T &data;
+    TreeNode *left, *right;
+  TreeNode(T &data) :
+    data(data), left(nullptr), right(nullptr) {}
+  };
+
+  TreeNode *root_;
+};
