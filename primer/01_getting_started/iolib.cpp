1acf5a935e00e65d321998a1268478d19f028f76
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-01-18 19:32:59 -0800

    init primer with 1/iolib

---
+++ primer/01_getting_started/iolib.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() {
+  int n1 = 0,
+    n2 = 0;
+
+  cout << "Enter 2 integers:" << endl;
+  cin >> n1 >> n2;
+  cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+
+  return 0;
+}
